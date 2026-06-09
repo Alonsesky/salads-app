1db41df1a4e45d4843fca4a02aaeeb56994a1846
Alo.gomezc <alo.gomezc@gmail.com>
2026-06-09 17:44:06 -0400

    Implementación Galeria de comentarios en Home

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { Footer } from "../../components/footer/footer";
 import { Navbar } from "../../components/navbar/navbar";
 import { Card } from "../../shared/components/card/card";
@@ -10,4 +10,35 @@ import { Nutri } from "../../components/nutri/nutri";
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements AfterViewInit {
+
+  @ViewChild('sliderTrack')
+  sliderTrack!: ElementRef<HTMLDivElement>;
+
+  currentIndex = 0;
+  totalSlides = 2;
+
+  ngAfterViewInit() {
+    this.totalSlides =
+      this.sliderTrack.nativeElement.children.length;
+
+    this.startAutoSlide();
+  }
+
+  nextSlide() {
+    this.currentIndex += 2;
+
+    if (this.currentIndex >= this.totalSlides) {
+      this.currentIndex = 0;
+    }
+
+    this.sliderTrack.nativeElement.style.transform =
+      `translateX(-${this.currentIndex * 50}%)`;
+  }
+
+  startAutoSlide(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 6000);
+  }
+}
